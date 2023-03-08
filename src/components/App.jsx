4b2f9165c6c7c14f31757c8056f516073ca77a99
Zub04ek <nastasyz@ukr.net>
2023-03-08 13:41:36 +0200

    remove fetch in app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,81 @@
 import { Component } from "react";
-import { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
 import { Container } from "./Container.styled";
 import { GlobalStyles } from "./Global.styled";
 import { ImageGallery } from "./ImageGallery/ImageGallery";
 import { Searchbar } from "./Searchbar/Searchbar";
+import { Button } from "components/LoadMoreBtn/LoadMoreBtn";
+import { Loader } from "components/Loader/Loader";
+import { Modal } from "components/Modal/Modal";
+import { fetchImages, limit } from "helpers/api";
 
 export class App extends Component {
   state = {
     searchQuery: '',
+    pictures: [],
+    page: 1,
+    loading: false,
+    showModal: false,
+    largeImg: '',
+    totalImages: 0,
   };
 
-  handleSubmit = searchQuery => {
-    this.setState({ searchQuery });
+  handleSubmit = newSearchQuery => {
+    this.state.searchQuery !== newSearchQuery && this.setState({ searchQuery: newSearchQuery, page: 1, pictures: [], totalImages: 0 });
+  }
+
+  async componentDidUpdate(prevProps, prevState) { 
+       try {
+           if (prevState.searchQuery !== this.state.searchQuery
+               || prevState.page !== this.state.page) {
+               
+             this.setState({ loading: true });
+             
+             const { hits, totalHits } = await fetchImages(this.state.searchQuery.trim(), this.state.page);
+             
+             if (hits.length === 0) {
+                    this.setState({ loading: false, pictures: [], totalImages: 0 });
+                    return toast.error(`Nothing found for ${this.state.searchQuery}!`);
+               };
+
+                this.setState({
+                    pictures: [...this.state.pictures, ...hits],
+                    loading: false,
+                    totalImages: totalHits,
+                });
+           };
+        } catch (err) {
+            console.log(err);
+       };
+  };
+  
+  handleLoad = () => {
+        this.setState(prev => ({ page: prev.page + 1 }));
+  }
+  
+  compareTotalPages = () => {
+    const totalPages = this.state.totalImages / limit;
+    const restTotalPages = this.state.pictures.length / limit;
+    return totalPages > restTotalPages;
+  }
+  
+  openModal = (e) => {
+        this.setState(({ showModal }) => ({
+            showModal: !showModal,
+        }));
+        this.setState({ largeImg: e.currentTarget.alt });
+    }
+
+  closeModal = () => {
+        this.setState(({ showModal }) => ({
+            showModal: !showModal,
+        }));
   }
 
   render() {
+    const { pictures, loading, showModal, largeImg, totalImages } = this.state;
+    const hasMorePages = this.compareTotalPages();
+
     return (
     <Container>
         <GlobalStyles />
@@ -23,7 +84,14 @@ export class App extends Component {
           reverseOrder={false}
         />
         <Searchbar onSubmit={this.handleSubmit} />
-        <ImageGallery searchQuery={this.state.searchQuery} />
+        <ImageGallery pictures={pictures} openModal={this.openModal} />
+        {showModal &&
+                    <Modal onClose={this.closeModal}>
+                        <img src={largeImg} alt={largeImg} width="800" />
+                    </Modal>
+                }
+        {loading && <Loader />}
+        {hasMorePages && totalImages > limit && <Button onClick={this.handleLoad}/>}
     </Container>
   );
   }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,103 +1,16 @@
-import { Button } from "components/LoadMoreBtn/LoadMoreBtn";
 import { ImageGalleryItem } from "components/ImageGalleryItem/ImageGalleryItem";
-import { Loader } from "components/Loader/Loader";
-import { Modal } from "components/Modal/Modal";
-import { fetchImages, limit } from "helpers/api";
-import { Component } from "react";
-import toast from 'react-hot-toast';
-import { List, Container } from "./ImageGallery.styled";
+import { List } from "./ImageGallery.styled";
 import PropTypes from 'prop-types';
 
-export class ImageGallery extends Component {
-    state = {
-        pictures: [],
-        page: 1,
-        loading: false,
-        showModal: false,
-        largeImg: '',
-        totalImages: 0,
-    };
-
-   async componentDidUpdate(prevProps, prevState) { 
-       try {
-           if (this.state.page !== 1
-               && prevProps.searchQuery === this.props.searchQuery
-               && prevState.page !== this.state.page) {
-               
-                this.setState({ loading: true });
-               const { hits } = await fetchImages(this.props.searchQuery.trim(), this.state.page);
-
-                this.setState({
-                    pictures: [...this.state.pictures, ...hits],
-                    loading: false
-                });
-           };
-           if (prevProps.searchQuery !== this.props.searchQuery) {
-               
-                this.setState({ loading: true });
-               const { hits, totalHits } = await fetchImages(this.props.searchQuery.trim());
-               
-                this.setState({
-                    pictures: hits,
-                    loading: false,
-                    page: 1,
-                    totalImages: totalHits,
-                });
-               
-               if (hits.length === 0) {
-                    return toast.error(`Nothing found for ${this.props.searchQuery}!`);
-               };
-               return;
-           };
-        } catch (err) {
-            console.log(err);
-       };
-    };
-
-    handleLoad = () => {
-        this.setState(prev => ({ page: prev.page + 1 }));
-    }
-    
-    openModal = (e) => {
-        this.setState(({ showModal }) => ({
-            showModal: !showModal,
-        }));
-        this.setState({ largeImg: e.currentTarget.alt });
-    }
-
-    closeModal = () => {
-        this.setState(({ showModal }) => ({
-            showModal: !showModal,
-        }));
-    }
-
-    compareTotalPages = () => {
-        const totalPages = this.state.totalImages / limit;
-        const restTotalPages = this.state.pictures.length / limit;
-        return totalPages > restTotalPages;
-    }
-
-    render() {
-        const { pictures, loading, showModal, largeImg, totalImages } = this.state;
-        const hasMorePages = this.compareTotalPages();
-
+export const ImageGallery = ({pictures, openModal}) => {
         return (
-            <Container>
                 <List>
-                    <ImageGalleryItem images={pictures} onClick={this.openModal} />
+                    <ImageGalleryItem images={pictures} onClick={openModal} />
                 </List>
-                {loading && <Loader />}
-                {showModal &&
-                    <Modal onClose={this.closeModal}>
-                        <img src={largeImg} alt={largeImg} width="800" />
-                    </Modal>
-                }
-                {hasMorePages && totalImages > limit && <Button onClick={this.handleLoad}/>}
-            </Container>
         )
-    }
 }
 
 ImageGallery.propTypes = {
-  searchQuery: PropTypes.string.isRequired,
+    pictures: PropTypes.array.isRequired,
+    openModal: PropTypes.func.isRequired,
 };
